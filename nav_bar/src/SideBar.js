61c96fc5ed3f3b9c3ddcba09f9884f94eb7fed7b
Dhruva Juloori <dhruva.juloori@oracle.com>
2019-10-18 23:39:18 -0400

    implement side bar

---
+++ nav_bar/src/SideBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+
+export default class SideBar extends Component{
+
+    render() {
+        const navItems =[{"name":"Home"},{"name":"Courses"},{"name":"Organizations"},{"name":"Activity"}];
+        const navListItems = navItems.map((navItem) => <li><a className="subnav-menu-links" href="#">{navItem.name}</a></li>)
+        return(
+            <div className="side-nav-container">
+                <div className="side-nav">
+                   <ul className="menu-items">
+                     {navListItems}  
+                   </ul> 
+                </div>
+            </div>
+        );
+    }
+}
